e1392752a3dad6978ebe24cecf75609d1a74b423
alagrin <alagrin92@github.ccs.neu.edu>
2019-11-16 16:44:31 -0800

    added DateTime, button, and display of date/time

---
+++ kioskmode/app/src/main/java/com/samsung/knox/example/kioskmode/DateTime.java
@@ -0,0 +1,28 @@
+package com.samsung.knox.example.kioskmode;
+
+import android.content.Context;
+
+import com.samsung.android.knox.EnterpriseDeviceManager;
+import com.samsung.android.knox.datetime.DateTimePolicy;
+import com.samsung.knox.example.kioskmode.MainActivity;
+
+import java.util.Date;
+
+public class DateTime {
+    public String dateTime;
+
+    /** Utility method to grab time data from visit */
+//    public String processTimeData() {
+//
+//        EnterpriseDeviceManager edm = EnterpriseDeviceManager.getInstance(this.application);
+//        DateTimePolicy policy = edm.getDateTimePolicy();
+//        Date date = policy.getDateTime();
+//        return date.toString();
+//    }
+
+
+//    public static void main(String[] args) {
+//        this.processTimeData()
+//    }
+
+}
